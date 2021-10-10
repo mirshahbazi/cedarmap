8fbb76116e5d4461afc3bca97e8644f5a59d08c1
mirshahbazi <mr.mirshahbazi@gmail.com>
2021-10-10 21:08:40 +0330

    update the source code

--- ios/Classes/Extensions.swift
+++ ios/Classes/Extensions.swift
@@ -32,19 +32,6 @@ extension CLLocation {
     }
 }
 
-extension CLHeading {
-    func toDict() -> [String: Any]? {
-        return ["magneticHeading": self.magneticHeading,
-                "trueHeading": self.trueHeading,
-                "headingAccuracy": self.headingAccuracy,
-                "x": self.x,
-                "y": self.y,
-                "z": self.z,
-                "timestamp": Int(self.timestamp.timeIntervalSince1970 * 1000),
-        ]
-    }
-}
-
 extension CLLocationCoordinate2D {
     func toArray()  -> [Double] {
         return [self.latitude, self.longitude]
@@ -96,26 +83,15 @@ extension UIColor {
             let start = hexString.index(hexString.startIndex, offsetBy: 1)
             let hexColor = hexString[start...]
             
-            let scanner = Scanner(string: String(hexColor))
-            var hexNumber: UInt64 = 0
-            
             if hexColor.count == 6 {
+                let scanner = Scanner(string: String(hexColor))
+                var hexNumber: UInt64 = 0
+                
                 if scanner.scanHexInt64(&hexNumber) {
                     r = CGFloat((hexNumber & 0xff0000) >> 16) / 255
                     g = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
                     b = CGFloat(hexNumber & 0x0000ff) / 255
                     a = 255
-                    
-                    self.init(red: r, green: g, blue: b, alpha: a)
-                    return
-                }
-            } 
-            else if hexColor.count == 8 {
-                if scanner.scanHexInt64(&hexNumber) {
-                    a = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    r = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    g = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    b = CGFloat(hexNumber & 0x000000ff) / 255
                     
                     self.init(red: r, green: g, blue: b, alpha: a)
                     return
